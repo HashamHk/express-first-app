89dfedba0cbb5938b5636ca29347bf8429f30991
HashamHk <60822100+HashamHk@users.noreply.github.com>
2020-10-29 17:09:53 +0500

    Issue resolved, it was VIEWS but i typed /VIEW/

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + './view/layouts/'}));
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
