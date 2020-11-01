e5cf5ba36b25af327419d475ab588460c3478926
HashamHk <60822100+HashamHk@users.noreply.github.com>
2020-11-01 13:20:05 +0500

    Successfully executed Validation-Sessions

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'form Validation', success: false, errors: req.session.errors });
+  res.render('index', { title: 'form Validation', success: req.session.success, errors: req.session.errors });
   req.session.errors = null;
 });
 
-router.post('/submit', function(re, res, next){
-  // CHeck validity
+router.post('/submit', function(req , res, next){
+  req.check('email', 'Invalid Email Adress !').isEmail();
+  req.check('password', 'Password is Invalid !').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors){
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
