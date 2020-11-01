16f82f145ac7e2d1a558c594199694384b1abe6b
HashamHk <60822100+HashamHk@users.noreply.github.com>
2020-11-01 17:48:13 +0500

    Backup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
-// const { get, route } = require('../app');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test'
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,18 +12,61 @@ router.get('/', function(req, res, next) {
   req.session.errors = null;
 });
 
-router.post('/submit', function(req , res, next){
-  req.check('email', 'Invalid Email Adress !').isEmail();
-  req.check('password', 'Password is Invalid !').isLength({min: 4}).equals(req.body.confirmPassword);
+router.get('/get-data' , function(req, res, next){
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err){
+      assert(null, err);
+      resultArray.push(doc);
+    }, function(){
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
 
-  var errors = req.validationErrors();
-  if (errors){
-    req.session.errors = errors;
-    req.session.success = false;
-  } else {
-    req.session.success = true;
+router.post('/insert' , function(req, res, next){
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
   }
+
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+
+  
+
   res.redirect('/');
 });
+router.post('/update' , function(req, res, next){
+
+});
+router.post('/delete' , function(req, res, next){
+
+});
+
+// router.post('/submit', function(req , res, next){
+//   req.check('email', 'Invalid Email Adress !').isEmail();
+//   req.check('password', 'Password is Invalid !').isLength({min: 4}).equals(req.body.confirmPassword);
+
+//   var errors = req.validationErrors();
+//   if (errors){
+//     req.session.errors = errors;
+//     req.session.success = false;
+//   } else {
+//     req.session.success = true;
+//   }
+//   res.redirect('/');
+// });
 
 module.exports = router;
